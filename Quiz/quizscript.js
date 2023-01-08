00b89d44e19c6e516811ef67d52a25c1bcba10de
jquery <jquery@jquery>
2023-01-08 22:50:23 +0100

    Quiz avec setTimeout

--- Quiz/quizscript.js
+++ Quiz/quizscript.js
@@ -2,7 +2,7 @@ const QUESTIONS = [
   {
     question: "En quelle année Google a-t-il été lancé sur le web ?",
     answers: [1998, 1990, 1899],
-    correctAnswer: 1998,
+    correctAnswer: "1998",
   },
   {
     question:
@@ -18,7 +18,7 @@ const QUESTIONS = [
   {
     question: "Que vaut:  3 + 0 * 5 - 2 ?",
     answers: [13, 6, 1],
-    correctAnswer: 1,
+    correctAnswer: "1",
   },
   {
     question: "Quel fruit a le même nom qu'un animal ?",
@@ -28,75 +28,120 @@ const QUESTIONS = [
   {
     question: "Combien de minutes dure un match de rugby ?",
     answers: [90, 75, 80],
-    correctAnswer: 80,
+    correctAnswer: "80",
   },
   {
     question: "Quel est le numéro de la maison des Simpson ?",
     answers: [720, 742, 801],
-    correctAnswer: 742,
+    correctAnswer: "742",
   },
   {
     question: " Qui a joué Harry Potter dans Harry Potter  ?",
     answers: ["Rupert Grint", "Alan Rickman", "Daniel Radcliffe"],
     correctAnswer: "Daniel Radcliffe",
   },
+  {
+    question: "Quelle est la capitale de la France?",
+    answers: ["Paris", "Londres", "Berlin"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "Combien y a-t-il de pays dans l'Union Européenne?",
+    answers: ["27", "28", "30"],
+    correctAnswer: "27",
+  },
 ];
 
 QUESTIONS.sort(() => Math.random() - 0.2);
-let correctAnswer = [];
-let jq = $.noConflict();
-jq(document).ready(function () {
-  for (i in QUESTIONS) {
-    if (i < 5) {
-      let div = jq(`<div id="question${i}"></div>`);
-      let paragraph = jq(`<p> ${QUESTIONS[i].question} </p>`);
-      let p = jq(`<p id="answer${i}"></p>`);
 
-      jq("#quiz").append(div);
-      jq(`#question${i}`).append(paragraph);
-      jq(`#question${i}`).append(p);
-      for (j in QUESTIONS[i].answers) {
-        let input = jq(
-          `<input type="radio" id="answers${i}${j}" value="${QUESTIONS[i].answers[j]}" name="answers${i}">
-          <label for="answers${i}${j}">
-           ${QUESTIONS[i].answers[j]}</label><br>`
-        );
-        jq(`#answer${i}`).append(input);
-        if (QUESTIONS[i].answers[j] == QUESTIONS[i].correctAnswer) {
-          correctAnswer.push([`answers${i}${j}${QUESTIONS[i].answers[j]}`]);
-        }
-      }
+let jq = $.noConflict();
+//let correctAnswer = [];
+let questions = [];
+
+jq(document).ready(function () {
+  let questionParagraph = jq("#question");
+  let answerParagraph = jq("#answers");
+  let currentQuestion = 0;
+
+  //Choose five questions
+  function chooseQuestions() {
+    for (i in QUESTIONS) if (i < 2) questions.push(QUESTIONS[i]);
+
+    return questions;
+  }
+
+  let quiz = chooseQuestions();
+
+  function displayQuestion() {
+    //retrieve the current question
+    const question = quiz[currentQuestion];
+
+    // display current question
+
+    questionParagraph.html(question.question);
+
+    // display choices
+    let input = "";
+    let j = 0;
+
+    for (const choice of question.answers) {
+      input += `<input type="radio" id="choice${currentQuestion}${j}" name="choice${currentQuestion}" value="${choice}">
+        <label for="choice${currentQuestion}${j}">
+         ${choice}</label><br>`;
+      j++;
     }
+    answerParagraph.html(input);
+  }
+
+  // Display border none for next question
+  function displayNone(id) {
+    jq(id).parent().parent().css({
+      border: "none",
+    });
   }
 
   let score = 0;
-  jq("input[type=radio]").on("change", function () {
-    let userAnswer = this.id + this.value;
-    let answer = checkAnswer(correctAnswer, userAnswer);
-    if (answer !== "") {
-      jq(`#${this.id}`)
-        .parent()
-        .parent()
-        .css({ border: "solid 5px green", "border-radius": "10px" });
-      jq(`#${this.id}`).siblings().attr("disabled", "true");
+  function checkAnswer(event) {
+    // checks if the clicked button is the correct answer
+    if (event.target.value === quiz[currentQuestion].correctAnswer) {
+      jq(`#${event.target.id}`).parent().parent().css({
+        border: "solid 2px green",
+      });
+      jq(`#${event.target.id}`).siblings("input").attr("disabled", "true");
+
       score += 1;
       jq("#score").text("Score: " + score);
+      setTimeout(displayNone, 1500, `#${event.target.id}`);
     } else {
-      jq(`#${this.id}`)
+      jq(`#${event.target.id}`)
         .parent()
         .parent()
-        .css({ border: "solid 5px red", "border-radius": "10px" });
-      jq(`#${this.id}`).siblings().attr("disabled", "true");
+        .css({ border: "solid 2px red", "border-radius": "10px" });
+      jq(`#${event.target.id}`).siblings("input").attr("disabled", "true");
+      setTimeout(displayNone, 1500, `#${event.target.id}`);
     }
-  });
-});
 
-function checkAnswer(correctAnswerArray, userAnswer) {
-  let answer = "";
-  correctAnswerArray.forEach((element) => {
-    for (let i in element) {
-      if (element[i] === userAnswer) answer = "correct";
+    currentQuestion++;
+    // check if the quiz is completed
+    if (currentQuestion === quiz.length) {
+      setTimeout(function () {
+        alert("Votre sccore est : " + score + " / " + quiz.length);
+        let button = jq(
+          '<button onclick="location.reload();">Jouer de nouveau</button>'
+        );
+        jq("body").append(button);
+      }, 300);
+    } else {
+      // following question in 1.5 second
+      setTimeout(displayQuestion, 1500);
+    }
+  }
+
+  document.addEventListener("click", (event) => {
+    if (event.target.matches("input[type=radio]")) {
+      checkAnswer(event);
     }
   });
-  return answer;
-}
+
+  displayQuestion();
+});
